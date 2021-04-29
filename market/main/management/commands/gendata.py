74b30d74de7898d6aa000a77dea05b0beb6b8cbe
Pavel <3410914@gmail.com>
2021-04-29 19:13:30 +0300

    11_3 done

--- market/main/management/commands/gendata.py
+++ market/main/management/commands/gendata.py
@@ -1,5 +1,23 @@
+import string
+
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from random import choices
+
+from main.models import Product
 
 
-class GenerateData(BaseCommand):
-    ...
+def generate_random_str():
+    return ''.join(choices(string.ascii_letters, k=5))
+
+
+class Command(BaseCommand):
+    """
+    Generates sample data
+    """
+    def handle(self, *args, **options):
+        username = generate_random_str()
+        User.objects.create(username=username)
+
+        product_title = generate_random_str()
+        Product.objects.create(title=product_title)
