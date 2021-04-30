422a120d6a3f019b2f2a7810fabebe1280ac1646
Pavel <3410914@gmail.com>
2021-04-30 17:33:46 +0300

    12_1 done

--- market/market/settings.py
+++ market/market/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
